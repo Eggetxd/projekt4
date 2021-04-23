4a0a03f98ea19c05b59347c106bec122e8d6fd1a
Adam Egnell <adam.egnell@hotmail.com>
2021-04-23 13:26:31 +0200

    gjorde om drop down menyn till en slide in meny och fick lite problem iconen men löste det

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 function dropDownClick() {
+  /*
   document.getElementById("dropDown").classList.toggle("show");
-  document.getElementById("dropDownButton").classList.toggle("buttonRadius");
+  */
+  document.getElementById("slideIn").classList.toggle("showSlideIn");
+  console.log("visar")
 }
-
+/*
 window.onclick = function(event) {
     if (!event.target.matches('.dropDownButton')) {
         var dropDown = document.getElementsByClassName("dropDownContent");
         dropDown[0].classList.remove('show');
+    }
+}
+*/
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropDownButton')) {
+        var slideIn = document.getElementsByClassName("slideInContent");
+        
+        slideIn[0].classList.remove('showSlideIn');
+        console.log("visa inte")
+        console.log(event.target)
+          
     }
 }
