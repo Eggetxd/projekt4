7ce35f69c6d6cb61e81d63f236ae0f2b64cefb66
Adam Egnell <adam.egnell@hotmail.com>
2021-04-28 11:07:43 +0200

    tog bort koden för drop down menyn och gjorde slide in snyggare

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
 function dropDownClick() {
-  /*
-  document.getElementById("dropDown").classList.toggle("show");
-  */
   document.getElementById("slideIn").classList.toggle("showSlideIn");
   console.log("visar")
 }
-/*
-window.onclick = function(event) {
-    if (!event.target.matches('.dropDownButton')) {
-        var dropDown = document.getElementsByClassName("dropDownContent");
-        dropDown[0].classList.remove('show');
-    }
-}
-*/
 
 window.onclick = function(event) {
-    if (!event.target.matches('.dropDownButton')) {
+    if (!event.target.matches('.slideInButton')) {
         var slideIn = document.getElementsByClassName("slideInContent");
         
         slideIn[0].classList.remove('showSlideIn');
