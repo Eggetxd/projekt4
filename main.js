b3487175645e8580d98cd6d67d469269c64db266
Adam Egnell <adam.egnell@hotmail.com>
2021-05-11 13:42:12 +0200

    Börjat på andra delsidan, fixat slideInContent position

--- main.js
+++ main.js
@@ -11,12 +11,11 @@ window.onclick = function(event) {
 
 
 let imageShown = 0;
-
+times = 0;
 function changePicture() {
   let images = ["https://picsum.photos/id/1015/" + window.innerWidth + "/" + ((window.innerHeight / 2).toFixed()), "https://picsum.photos/id/1018/" + window.innerWidth + "/" + (window.innerHeight * 2 / 3).toFixed(), "https://picsum.photos/id/1016/" + window.innerWidth + "/" + (window.innerHeight * 2 / 3).toFixed()];
 
   document.getElementById("changingImage").src=images[imageShown];
-
   if (imageShown == 2) {
     imageShown = 0;
   } else {
@@ -24,4 +23,4 @@ function changePicture() {
   }
 }
 changePicture();
-setInterval(changePicture, 2000);
+setInterval(changePicture, 3000);
