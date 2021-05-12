e63b35304eb57533174d0247ded5c7bb777bdddf
Adam Egnell <adam.egnell@hotmail.com>
2021-05-12 11:00:58 +0200

    fixade till variabler för färgerna, fixade andra delsidan och började på footern

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function changePicture() {
   let images = ["https://picsum.photos/id/1015/" + window.innerWidth + "/" + ((window.innerHeight / 2).toFixed()), "https://picsum.photos/id/1018/" + window.innerWidth + "/" + (window.innerHeight * 2 / 3).toFixed(), "https://picsum.photos/id/1016/" + window.innerWidth + "/" + (window.innerHeight * 2 / 3).toFixed()];
 
   document.getElementById("changingImage").src=images[imageShown];
+
   if (imageShown == 2) {
     imageShown = 0;
   } else {
